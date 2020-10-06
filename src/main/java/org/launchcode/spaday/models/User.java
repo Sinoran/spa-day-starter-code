33b99fc26c4f51a86225a3e563469c69e1a65764
Scott Sullentrop <sscalex@yahoo.com>
2020-10-06 09:40:56 -0500

    Spa Day Studio working as intended!

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,19 +1,32 @@
 package org.launchcode.spaday.models;
 
+import javax.swing.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+
 public class User {
+    @Size(min = 5, max = 15)
+    @NotBlank(message = "You have to type something.")
     private String username;
+    @Email
     private String email;
+    @Size(min = 6, max = 15)
     private String password;
+    @NotBlank(message = "Please type something here.")
+    private String verify;
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verify) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verify = verify;
     }
 
     public String getUsername() {
@@ -38,5 +51,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getVerify() {
+        return verify;
+    }
+
+    public void setVerify(String verify) {
+        this.verify = verify;
     }
 }
